3bcb3d706d89f2208c962aa83b7e93f40c8ed5a4
YoumnaRefaat <youmnarefaat884@gmail.com>
2024-10-06 15:19:11 +0300

    register,login and chat pages

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,152 @@
+import 'package:chat_app/helper/show_snackbar.dart';
+import 'package:chat_app/screens/chat_page.dart';
+import 'package:chat_app/screens/register_page.dart';
+import 'package:chat_app/constants.dart';
+import 'package:chat_app/widgets/custom_button.dart';
+import 'package:chat_app/widgets/custom_text_form_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+  static String id = 'loginPage';
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isLoading = false;
+
+  GlobalKey<FormState> formkey = GlobalKey();
+  String? email, password;
+  @override
+  Widget build(BuildContext context) {
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: kPrimaryColor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 60),
+          child: Form(
+            key: formkey,
+            child: ListView(
+              children: [
+                const SizedBox(
+                  height: 60,
+                ),
+                Image.asset(
+                  kLogo,
+                  height: 100,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Scholar Chat',
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontFamily: 'pacifico',
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                const Row(
+                  children: [
+                    Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomTextFormField(
+                  onChanged: (data) {
+                    email = data;
+                  },
+                  hintText: 'EMAIL',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextFormField(
+                  obscureText: true,
+                  onChanged: (data) {
+                    password = data;
+                  },
+                  hintText: 'PASSWORD',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomButton(
+                  onTap: () async {
+                    if (formkey.currentState!.validate()) {
+                      isLoading = true;
+                      setState(() {});
+                      try {
+                        await loginUser();
+                        Navigator.pushNamed(context, ChatPage.id,
+                            arguments: email);
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'user-not-found') {
+                          showSnackBar(context, 'user nor found');
+                        } else if (e.code == 'wrong-password') {
+                          showSnackBar(context, 'wrong password');
+                        }
+                      } catch (e) {
+                        print(e);
+                        showSnackBar(context, 'there was an error');
+                      }
+                      isLoading = false;
+                      setState(() {});
+                    } else {}
+                  },
+                  text: 'LOGIN',
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'don\'t have an account ?',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, RegisterPage.id);
+                      },
+                      child: const Text(
+                        ' Register',
+                        style: TextStyle(
+                          color: Color(0xffC7EDE6),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> loginUser() async {
+    UserCredential user = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email!, password: password!);
+  }
+}
---
+++ lib/screens/register_page.dart
@@ -0,0 +1,157 @@
+import 'package:chat_app/helper/show_snackbar.dart';
+import 'package:chat_app/screens/chat_page.dart';
+import 'package:chat_app/constants.dart';
+import 'package:chat_app/widgets/custom_button.dart';
+import 'package:chat_app/widgets/custom_text_form_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+  static String id = 'RegisterPage';
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  String? email;
+
+  String? password;
+
+  bool isLoading = false;
+
+  GlobalKey<FormState> formkey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: kPrimaryColor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 60),
+          child: Form(
+            key: formkey,
+            child: ListView(
+              children: [
+                const SizedBox(
+                  height: 60,
+                ),
+                Image.asset(
+                  kLogo,
+                  height: 100,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Scholar Chat',
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontFamily: 'pacifico',
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                const Row(
+                  children: [
+                    Text(
+                      'REGISTER',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomTextFormField(
+                  onChanged: (data) {
+                    email = data;
+                  },
+                  hintText: 'EMAIL',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextFormField(
+                  onChanged: (data) {
+                    password = data;
+                  },
+                  hintText: 'PASSWORD',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomButton(
+                  onTap: () async {
+                    if (formkey.currentState!.validate()) {
+                      isLoading = true;
+                      setState(() {});
+                      try {
+                        await registerUser();
+                        Navigator.pushNamed(context, ChatPage.id,
+                            arguments: email);
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'weak-password') {
+                          showSnackBar(context, 'weak password');
+                        } else if (e.code == 'email-already-in-use') {
+                          showSnackBar(context, 'email already exists');
+                        }
+                      } catch (e) {
+                        showSnackBar(context, 'there was an error');
+                      }
+                      isLoading = false;
+                      setState(() {});
+                      showSnackBar(context, 'success');
+                    }
+                  },
+                  text: 'Register',
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'already have an account ?',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        ' Login',
+                        style: TextStyle(
+                          color: Color(0xffC7EDE6),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const Spacer(
+                  flex: 3,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> registerUser() async {
+    UserCredential user = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email!, password: password!);
+  }
+}
