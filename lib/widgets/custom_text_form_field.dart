525fab145d352f2a66486e7988074d587cd90259
YoumnaRefaat <youmnarefaat884@gmail.com>
2024-10-06 15:06:41 +0300

    custom text form field widget

---
+++ lib/widgets/custom_text_form_field.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  CustomTextFormField(
+      {super.key, this.obscureText = false, this.onChanged, this.hintText});
+  Function(String)? onChanged;
+  String? hintText;
+  bool? obscureText;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: obscureText!,
+      validator: (data) {
+        if (data!.isEmpty) {
+          return "field is required";
+        }
+        return null;
+      },
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          color: Colors.white,
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
