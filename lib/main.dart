16d9cfea42884bbfedc59e60f358af49a9596669
YoumnaRefaat <youmnarefaat884@gmail.com>
2024-10-06 15:21:29 +0300

    modified main file and finish scholar chat app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,31 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/screens/chat_page.dart';
+import 'package:chat_app/screens/login_page.dart';
+import 'package:chat_app/screens/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const ChatApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ScholarChatApp());
 }
 
-class ChatApp extends StatelessWidget {
-  const ChatApp({super.key});
+class ScholarChatApp extends StatelessWidget {
+  const ScholarChatApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        LoginPage.id: (context) => const LoginPage(),
+        RegisterPage.id: (context) => const RegisterPage(),
+        ChatPage.id: (context) => ChatPage()
+      },
+      initialRoute: LoginPage.id,
+    );
   }
 }
